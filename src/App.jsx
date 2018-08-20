103ee2c168523878d17061f5e1c27b7ca52a1400
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2018-08-20 18:34:29 +0800

    combine all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import styled from 'styled-components';
+import { inject, observer } from 'mobx-react';
+import TitleBar from './components/TitleBar';
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
+import FloatingButton from './components/FloatingButton';
+import eraser from './assets/images/eraser';
+import colors from './assets/themes/colors';
+import reset from './assets/images/reset';
 
-const StyledDiv = styled.div`
-  text-align: center;
-`;
-
-export default () => (
-  <StyledDiv>
-    <h1>Stark TODO</h1>
-  </StyledDiv>
+export default inject('taskStore')(
+  observer((props) => {
+    const { taskStore } = props;
+    return (
+      <div>
+        <TitleBar />
+        <TaskInput />
+        <TaskList />
+        <FloatingButton
+          background={eraser}
+          bgColor={colors.accent}
+          hoverBG={colors.darkAccent}
+          borderColor={colors.accent}
+          right="90px"
+          action={() => taskStore.clearFinishedItems()}
+        />
+        <FloatingButton
+          background={reset}
+          hoverBG={colors.darkWhite}
+          bgColor={colors.white}
+          borderColor={colors.accent}
+          right="20px"
+          action={() => taskStore.resetTODO()}
+        />
+      </div>
+    );
+  }),
 );
