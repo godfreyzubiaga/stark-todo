e6c461ca5055502bb41343112748b9d5266e2c8e
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2018-08-20 18:30:44 +0800

    add images url's

---
+++ src/assets/images/eraser.js
@@ -0,0 +1,1 @@
+export default 'http://res.cloudinary.com/starksten/image/upload/v1534758207/todo-app/eraser.svg';
---
+++ src/assets/images/reset.js
@@ -0,0 +1,1 @@
+export default 'http://res.cloudinary.com/starksten/image/upload/v1534758382/todo-app/reload-arrow.svg';
