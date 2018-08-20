2f0de587e881901531704655739aeb054767ea83
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2018-08-20 18:34:17 +0800

    inject store to app

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'mobx-react';
 import App from './App';
 import globalTheme from './assets/themes/globalTheme';
+import RootStore from './stores/RootStore';
 
 const mountPoint = document.getElementById('root');
+const store = new RootStore();
 
 globalTheme();
 
-render(<App />, mountPoint);
+render(
+  <Provider {...store}>
+    <App />
+  </Provider>,
+  mountPoint,
+);
